fded0bda0edacf957f2d84e152b7e8d8b38c44bb
Hrishabh Gohiya <hrishabh.gohiya@Yash.com>
2023-04-13 12:36:52 +0530

    updates

--- TypeScript/lib/type_script-stack.ts
+++ TypeScript/lib/type_script-stack.ts
@@ -1,101 +1,59 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { aws_apigateway } from 'aws-cdk-lib';
-import { aws_lambda } from 'aws-cdk-lib';
-import { aws_dynamodb } from 'aws-cdk-lib';
-import * as path from 'path';
+
+import { Stage } from 'aws-cdk-lib';
+import { Stack } from 'aws-cdk-lib';
+import { pipelines } from 'aws-cdk-lib';
+import { Environment } from 'aws-cdk-lib';
+
+import { ResourceStack } from './resource_stack/resource_stack';
+
 
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class TypeScriptStack extends cdk.Stack {
+export class DeployStage extends Stage {
+  constructor(scope: Construct, id: string, props?: cdk.StageProps) {
+    super(scope, id, props),
+      new ResourceStack(this, 'ResourceStack', { stackName: 'aws_resource_stack' });
+  }
+
+}
+
+export class TypeScriptStack extends Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-
-    // Creating Table for DynamoDB
-    const tableBooks = new aws_dynamodb.Table(this, 'Books', {
-      partitionKey: { name: 'book_id', type: aws_dynamodb.AttributeType.STRING },
-    });
-
-    // API Gateway Integration
-    const api = new aws_apigateway.RestApi(this, 'books-api',{});
-
-    // Resources for HTTP API methods
-    const BooksResource  = api.root.addResource('books')
-    const BookWithID = BooksResource.addResource("{book_id}")
-
-    // Lambda function to fetch All the Books
-    const listBooks = new aws_lambda.Function(this, 'listBooks',{
-      runtime: aws_lambda.Runtime.NODEJS_16_X,
-      handler: 'get.GetHandler',
-      code: aws_lambda.Code.fromAsset(path.join(__dirname, 'lambda_handler')),
-      environment: {
-        Books_Table_Name: tableBooks.tableName
-      }
-    })
-    // Granting permission to the lambda function
-    tableBooks.grantReadData(listBooks)
-
-    // GET method integration with lambda function
-    BooksResource.addMethod('GET',new aws_apigateway.LambdaIntegration(listBooks))
-
-
-   // Lambda function to Post books
-    const postBooks = new aws_lambda.Function(this, 'postBooks',{
-      runtime: aws_lambda.Runtime.NODEJS_16_X,
-      handler: 'post.PostHandler',
-      code: aws_lambda.Code.fromAsset(path.join(__dirname, 'lambda_handler')),
-      environment: {
-        Books_Table_Name: tableBooks.tableName
-      }
-    })
-    // Granting permission to the lambda function
-    tableBooks.grantReadWriteData(postBooks)
-
-    // POST method integration with lambda function
-    BooksResource.addMethod('POST',new aws_apigateway.LambdaIntegration(postBooks))
-
-    // Lambda function to fetch Books by Id
-    const BookById = new aws_lambda.Function(this, 'BookById',{
-      runtime: aws_lambda.Runtime.NODEJS_16_X,
-      handler: 'getById.GetByIdHandler',
-      code: aws_lambda.Code.fromAsset(path.join(__dirname, 'lambda_handler')),
-      environment: {
-        Books_Table_Name: tableBooks.tableName
-      }
-    })
-    tableBooks.grantReadData(BookById)
-    BookWithID.addMethod('GET',new aws_apigateway.LambdaIntegration(BookById))
-
-
-
-    // Lambda function to update Books by Id
-    const updateBooks = new aws_lambda.Function(this, 'updateBooks',{
-      runtime: aws_lambda.Runtime.NODEJS_16_X,
-      handler: 'update.UpdateHandler',
-      code: aws_lambda.Code.fromAsset(path.join(__dirname, 'lambda_handler')),
-      environment: {
-        Books_Table_Name: tableBooks.tableName
-      }
-    })
-
-    tableBooks.grantReadWriteData(updateBooks)
-    BookWithID.addMethod('PUT',new aws_apigateway.LambdaIntegration(updateBooks))
-
-
-    const deleteBooks = new aws_lambda.Function(this, 'deleteBooks',{
-      runtime: aws_lambda.Runtime.NODEJS_16_X,
-      handler: 'delete.DeleteHandler',
-      code: aws_lambda.Code.fromAsset(path.join(__dirname, 'lambda_handler')),
-      environment: {
-        Books_Table_Name: tableBooks.tableName
-      }
-    })
-
-    tableBooks.grantReadWriteData(deleteBooks)
-    BookWithID.addMethod('DELETE',new aws_apigateway.LambdaIntegration(deleteBooks))
-
   }
+  // The code that defines your stack goes here
+  git_source = pipelines.CodePipelineSource.connection(
+    'hris699/AWS_CDK_mini_project',
+    'main', {
+    connectionArn: 'arn:aws:codestar-connections:ap-south-1:916041404420:connection/3d7eb5e6-f1dc-407c-814b-c51e6568a462'
+  })
+
+  
+
+  synth_step = new pipelines.ShellStep("Synth", {
+    commands: [
+      'npm install',
+      'npx cdk synth'
+    ],
+    input: this.git_source
+  }
+  )
+
+  pipeline = new pipelines.CodePipeline(
+    this, 'CodePipeline',{
+    selfMutation: true,
+    synth:this.synth_step,
+    pipelineName: 'cdk_pipeline'
+    },
+    
+)
+
+deployment_wave = this.pipeline.addWave("DeploymentWave")
+
+stage = this.deployment_wave.addStage(new DeployStage(
+  this, 'DeployStage',{env: {account:'916041404420', region:'us-east-1'}})
+
+)
 }
